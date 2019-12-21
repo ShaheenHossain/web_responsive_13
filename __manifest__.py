6430efab0a88bf77b74966af5692e96309f22f56
shaheen hossain <rapidgrps@gmail.com>
2019-12-21 19:54:32 +0600

    web_responsive for eagle 13

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    "name": "Web Responsive 13",
+    "summary": "Responsive web client, community-supported",
+    "version": "13.0.1.0.1",
+    "category": "Website",
+    "website": "https://eagle-erp.com",
+    "author": "LasLabs, Tecnativa, " "Eagle ERP",
+    "installable": True,
+    "depends": ["web", "mail"],
+    "development_status": "Production/Stable",
+    "maintainers": ["rapidgrps@gmail.com"],
+    "data": ["views/assets.xml", "views/res_users.xml", "views/web.xml"],
+    "qweb": [
+        "static/src/xml/apps.xml",
+        "static/src/xml/form_view.xml",
+        "static/src/xml/navbar.xml",
+    ],
+}
